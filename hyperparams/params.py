8707f5e32c4e65250b9bcd8fdee768ce3c2e33cb
Mayank Kumar Pal <mynkpl1998@gmail.com>
2023-08-25 22:09:31 +0530

    added hyperparams for BipedalWalker-v3

--- hyperparams/params.py
+++ hyperparams/params.py
@@ -27,4 +27,34 @@ PARAMS = {
             'logger_title': 'test_logger' 
         },
     },
+
+    "BipedalWalker-v3": 
+    {
+        "DDPG": 
+        {
+            'env_id': "BipedalWalker-v3",
+            'seed': 258,
+            'replay_size': int(1e6),
+            'polyak': 0.995,
+            'actor_critic_hidden_size': 256,
+            'activation': 'relu',
+            'update_batch_size': 256,
+            'update_frequency': int(500),
+            'update_iterations': 4,
+            'gamma': 0.98,
+            'n_step': 3,
+            'critic_lr':1e-4,
+            'actor_lr': 1e-4,
+            'critic_loss': 'hubber',
+            'num_training_episodes': int(50000),
+            'exploration_noise_scale': 0.1,
+            'warm_up_iters': 10000,
+            'max_gradient_norm': 1.0,
+            'num_test_episodes': 20,
+            'evaluation_freq_episodes': 100,
+            'normalize_observations': True,
+            'enable_wandb_logging': True,
+            'logger_title': 'test_logger'
+        },
+    }
 }
